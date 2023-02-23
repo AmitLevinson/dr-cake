5d21b64be18f4af8437a43911a82bfb64cc7a92e
Amit Levinson <amitlevinson1@gmail.com>
2023-02-23 07:30:16 +0200

    add css, fix search bar and elements positions

--- src.js
+++ src.js
@@ -1,16 +1,32 @@
 // https://css-tricks.com/in-page-filtered-search-with-vanilla-javascript/
-let cards = document.querySelectorAll('.card')
+// let cards = document.querySelectorAll('div.card');
+// console.log(cards)
+// var cards = [...document.querySelectorAll("div")];
+// console.log(cards.length);
+// var elms = document.querySelectorAll('selectors');
 
 function liveSearch () {
-    let searchQuery = document.getElementById("searchbox").value;
-    console.log(searchQuery)
+    var input, filter, cards, cardContainer, text, i;
+    input = document.getElementById('searchbox');
+    filter = input.value.toUpperCase();
+    cardContainer = document.getElementById('myCakes')
+    cards = cardContainer.getElementsByClassName('card');
+    for(i = 0; i < cards.length; i ++) {
+        text = cards[i].querySelector('.card-body');
+        if(text.innerText.toUpperCase().indexOf(filter) > -1) {
+            cards[i].style.display = "";
+        } else {
+            cards[i].style.display = "none";
+        }
+    }
 }
-let typingTimer;
-let typeInterval = 500;
-let searchInput = document.getElementById('searchbox');
 
-searchInput.addEventListener('keyup', () => {
-    clearTimeout(typingTimer);
-    typingTimer = setTimeout(liveSearch, typeInterval);
-})
+// let typingTimer;
+// let typeInterval = 500;
+// let searchInput = document.getElementById('searchbox');
+
+// searchInput.addEventListener('keyup', () => {
+//     clearTimeout(typingTimer);
+//     typingTimer = setTimeout(liveSearch, typeInterval);
+// })
 
