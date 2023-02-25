48a32eac34560e6188b01e23b15e07df6dc6a269
Amit Levinson <amitlevinson1@gmail.com>
2023-02-25 20:47:11 +0200

    clean up js

--- src.js
+++ src.js
@@ -1,9 +1,4 @@
 // https://css-tricks.com/in-page-filtered-search-with-vanilla-javascript/
-// let cards = document.querySelectorAll('div.card');
-// console.log(cards)
-// var cards = [...document.querySelectorAll("div")];
-// console.log(cards.length);
-// var elms = document.querySelectorAll('selectors');
 
 function liveSearch () {
     var input, filter, cards, cardContainer, text, i;
@@ -20,13 +15,3 @@ function liveSearch () {
         }
     }
 }
-
-// let typingTimer;
-// let typeInterval = 500;
-// let searchInput = document.getElementById('searchbox');
-
-// searchInput.addEventListener('keyup', () => {
-//     clearTimeout(typingTimer);
-//     typingTimer = setTimeout(liveSearch, typeInterval);
-// })
-
